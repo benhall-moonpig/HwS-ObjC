b1d77e034e08230c3e6266b722191eee9f8c829b
Ben Hall <ben.hall@moonpig.com>
2019-11-15 12:21:37 +0000

    Adds NSNumber and NSArray stuff

---
+++ BasicSyntax/BasicSyntax/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  BasicSyntax
+//
+//  Created by Ben Hall on 15/11/2019.
+//  Copyright © 2019 Ben Hall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+        
+#pragma mark NSNumber
+        NSNumber *doubleNum = @M_PI;
+        NSNumber *intNum = @10;
+        NSNumber *boolNum = @YES;
+        
+        NSLog(@"%3.2f", [doubleNum floatValue]);
+        NSLog(@"%ld", (long)[intNum integerValue]);
+        NSLog(@"%d", [boolNum intValue]);
+        
+#pragma mark Arrays
+        NSArray *characters = @[@"Chewie", @"Han", @"Leia"];
+        for (NSString *c in characters) {
+            NSLog(@"%@", c);
+        }
+        
+    }
+    return 0;
+}
