a3953fede061dd7aa61fb7fcf03570128d8cd6a0
Ben Hall <ben.hall@moonpig.com>
2019-11-16 14:38:11 +0000

    Adds basic game logic

--- Hangman/Hangman/main.m
+++ Hangman/Hangman/main.m
@@ -20,7 +20,34 @@ int main(int argc, const char * argv[]) {
         
         printf("Welcome to Hangman!\n");
         printf("Press a letter to guess, or Ctrl+C to quit.\n");
-        printWord(word, usedLetters);
+        
+        while(1) {
+            // Read cstring from user
+            char cstring[256];
+            scanf("%s", cstring);
+            
+            // Convert to NSString for use here
+            NSString *input = [NSString stringWithCString:cstring encoding:NSUTF8StringEncoding];
+            
+            if ([input length] != 1) {
+                printf("Please type exactly one letter, or Ctrl+C to quit.");
+            } else {
+                // Read first unichar character in converted NSString input
+                unichar letter = [input characterAtIndex:0];
+                // Convert to uppercase NSString
+                NSString *upperCasedLetter = [[NSString stringWithFormat:@"%C", letter] uppercaseString];
+                // Check to see if letter exists
+                BOOL letterHasBeenUsed = [usedLetters containsObject:upperCasedLetter];
+                // If exists - print warning
+                if (letterHasBeenUsed) {
+                    printf("This letter has already been used. Please try another one!\n");
+                } else { // Else add letter
+                    [usedLetters addObject:upperCasedLetter];
+                }
+            }
+            // Print the game state
+            printWord(word, usedLetters);
+        }
     }
     return 0;
 }
