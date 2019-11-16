11a52db9fc6c458ee44d2ba9f9e9901a8868451d
Ben Hall <ben.hall@moonpig.com>
2019-11-16 15:02:04 +0000

    Adds example Class to BasicSyntax project
    Person class

---
+++ BasicSyntax/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  BasicSyntax
+//
+//  Created by Ben Hall on 16/11/2019.
+//  Copyright © 2019 Ben Hall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BasicSyntax/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  BasicSyntax
+//
+//  Created by Ben Hall on 16/11/2019.
+//  Copyright © 2019 Ben Hall. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
