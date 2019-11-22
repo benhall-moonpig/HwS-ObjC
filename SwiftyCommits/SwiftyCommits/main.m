735bb2a7d165e92154d3ad0a83989bba8b95af2b
Ben Hall <ben.hall@moonpig.com>
2019-11-22 16:52:08 +0000

    Adds logic to main for taking input repo and printing commits
    - Defaults to apple/swift repo

--- SwiftyCommits/SwiftyCommits/main.m
+++ SwiftyCommits/SwiftyCommits/main.m
@@ -7,11 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SCApp.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSString *repo = @"apple/swift";
+        
+        if (argc == 2) {
+            repo = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+        }
+        
+        SCApp *app = [SCApp new];
+        [app fetchCommitsForRepo:repo];
     }
     return 0;
 }
