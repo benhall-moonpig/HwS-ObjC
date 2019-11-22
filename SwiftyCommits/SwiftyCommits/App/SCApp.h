88fff428fbb65a702c066fc98bd9c3104432bea0
Ben Hall <ben.hall@moonpig.com>
2019-11-22 16:36:23 +0000

    Adds SCApp class for fetching github commit message and printing

---
+++ SwiftyCommits/SwiftyCommits/App/SCApp.h
@@ -0,0 +1,19 @@
+//
+//  SCApp.h
+//  SwiftyCommits
+//
+//  Created by Ben Hall on 22/11/2019.
+//  Copyright © 2019 Ben Hall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SCApp : NSObject
+
+- (void)fetchCommitsForRepo:(NSString*)repo;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SwiftyCommits/SwiftyCommits/App/SCApp.m
@@ -0,0 +1,46 @@
+//
+//  SCApp.m
+//  SwiftyCommits
+//
+//  Created by Ben Hall on 22/11/2019.
+//  Copyright © 2019 Ben Hall. All rights reserved.
+//
+
+#import "SCApp.h"
+
+@implementation SCApp
+
+- (void)fetchCommitsForRepo:(NSString *)repo {
+    NSString *urlString = [NSString stringWithFormat:@"https://api.github.com/repos/%@/commits", repo];
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    // Get data from URL
+    NSError *requestError;
+    NSData *data = [NSData dataWithContentsOfURL:url options:NSDataReadingUncached error:&requestError];
+    
+    if (requestError) {
+        NSLog(@"Fatal error 1: %@", [requestError localizedDescription]);
+        exit(0);
+    }
+    
+    // Parse data
+    NSError *parsingError;
+    NSArray *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parsingError];
+    
+    if (parsingError) {
+        NSLog(@"Fatal Error 2: %@", [parsingError localizedDescription]);
+        exit(0);
+    }
+    
+    for (NSDictionary *entry in json) {
+        NSString *author = entry[@"commit"][@"author"][@"name"];
+        NSString *message = entry[@"commit"][@"message"];
+        
+        message = [message stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+        
+        printf("%s: %s\n\n", [author cStringUsingEncoding:NSUTF8StringEncoding], [message cStringUsingEncoding:NSUTF8StringEncoding]);
+    }
+}
+
+@end
