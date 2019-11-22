b51fd8e921c52870424ab032c2630b03ad85cfd7
Ben Hall <ben.hall@moonpig.com>
2019-11-22 12:14:16 +0000

    Creates SciFiQuotes project (CLT)

---
+++ SciFiQuotes/SciFiQuotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SciFiQuotes
+//
+//  Created by Ben Hall on 22/11/2019.
+//  Copyright © 2019 Ben Hall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
